b7b70da4f67d48ea0b3c458ea8ae790b8531e721
jinsejeong <tpwlswjd913@naver.com>
2025-02-17 13:32:25 +0900

    Title: 1시간 뒤 시간 출력, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 250217/1시간 뒤 시간 출력/print-one-hour-later.java
+++ 250217/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        sc.useDelimiter(":");
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        System.out.printf("%d:%d",a,b);
+
+        String time = sc.next();
+
+        String[] t = time.split(":");
+        int a = Integer.parseInt(t[0]);
+        int b = Integer.parseInt(t[1]);
+        System.out.printf("%d:%d",a+1,b);
     }
 }
