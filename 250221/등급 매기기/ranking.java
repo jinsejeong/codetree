2861bd72e1a7a1ff776e51af56ca0b911e5c1484
jinsejeong <tpwlswjd913@naver.com>
2025-02-21 12:06:25 +0900

    Title: 등급 매기기, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250221/등급 매기기/ranking.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+        score /= 10;
+        switch(score) {
+            case 9 :
+                System.out.print("A");
+                break;
+            case 8 :
+                System.out.print("B");
+                break;
+            case 7 :
+                System.out.print("C");
+                break;
+            case 6 :
+                System.out.print("D");
+                break;
+            default :
+                System.out.print("F");
+                break;
+        }
+    }
+}
