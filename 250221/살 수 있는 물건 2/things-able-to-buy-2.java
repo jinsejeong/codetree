932d1e1eb4708fb373cffe76ad6f3c9b9363fcb0
jinsejeong <tpwlswjd913@naver.com>
2025-02-21 12:19:46 +0900

    Title: 살 수 있는 물건 2, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250221/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int N = sc.nextInt();
+
+        if(N>=3000) {
+            System.out.print("book");
+        }
+        else if(N>=1000) {
+            System.out.print("mask");
+        }
+        else if(N>=500) {
+            System.out.print("pen");
+        }
+        else 
+            System.out.print("no");
+    }
+}
