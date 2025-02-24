a611e270f67ce7df1dd17bdf297bb753556a3bb9
jinsejeong <tpwlswjd913@naver.com>
2025-02-24 16:20:35 +0900

    Title: 두 사람, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 250224/두 사람/two-person.java
+++ 250224/두 사람/two-person.java
@@ -8,18 +8,11 @@ public class Main {
         int bAge = sc.nextInt();
         String bGender = sc.next();
 
-        if(aAge < 19 && bAge <19) {
+        if(aAge>=19 && aGender.equals("M"))
+            System.out.print("1");
+        else if (bAge>=19 && bGender.equals("M"))
+            System.out.print("1");
+        else 
             System.out.print("0");
-        }
-        else {
-            if (aGender.equals("W") && bGender.equals("W"))
-                System.out.print("0");
-            else if (aGender.equals("W") && bGender.equals("M"))
-                System.out.print("1");
-            else if (aGender.equals("M") && bGender.equals("W"))
-                System.out.print("1");
-            else 
-                System.out.print("1");
-        }
     }
 }
