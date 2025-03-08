9087810db155bdeb46eb6b13fad04fca2f9d1fc3
jinsejeong <tpwlswjd913@naver.com>
2025-03-08 21:59:31 +0900

    Title: 증가시키며 출력하기 3, Time: 86ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250308/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int N =10;
+        while (N<=26) {
+            System.out.print(N+" ");
+            N+=2;
+        }
+    }
+}
