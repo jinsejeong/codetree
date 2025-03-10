55dbfbf5b4cd854f6e2c271115b5113cad91b87c
jinsejeong <tpwlswjd913@naver.com>
2025-03-10 22:41:00 +0900

    Title: 높은 수에서 낮은 수까지, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 250310/높은 수에서 낮은 수까지/from-high-to-low.java
+++ 250310/높은 수에서 낮은 수까지/from-high-to-low.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
         int B = sc.nextInt();
-        if(A>B){
+        if(A>=B){
             for(int i=A; i>=B; i--) {
                 System.out.print(i+" ");
             }
