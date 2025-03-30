2d23513785291c0c4f0f97a78cb637d0176a335e
jinsejeong <tpwlswjd913@naver.com>
2025-03-30 21:46:40 +0900

    Title: 조건에 따른 연산, Time: 106ms, Memory: 10MB, Status: Passed - Codetree

--- 250330/조건에 따른 연산/operatino-by-rule.java
+++ 250330/조건에 따른 연산/operatino-by-rule.java
@@ -8,7 +8,7 @@ public class Main {
             if(n%2==0) {
                 n=n*3+1;
                 cnt++;}
-            if(n%2!=0){
+            else if(n%2!=0){
                 n=n*2+2;
                 cnt++;}
 
