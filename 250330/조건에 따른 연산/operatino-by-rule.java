f6be33f9f4f8cddae8ffd98887fcf8bfa7163f02
jinsejeong <tpwlswjd913@naver.com>
2025-03-30 21:45:26 +0900

    Title: 조건에 따른 연산, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250330/조건에 따른 연산/operatino-by-rule.java
+++ 250330/조건에 따른 연산/operatino-by-rule.java
@@ -5,15 +5,17 @@ public class Main {
         int n = sc.nextInt();
         int cnt =0;
         while(true) {
-            if(n>=1000) 
-                break;
-
             if(n%2==0) {
                 n=n*3+1;
                 cnt++;}
             if(n%2!=0){
                 n=n*2+2;
                 cnt++;}
+
+            if(n>=1000) 
+                break;
+
+            
         }
         System.out.print(cnt);
     }
